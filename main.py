e99c38f2e67b590071963acfec14e0ed5940813d
georgborn <georg.born@posteo.de>
2025-01-06 14:05:29 +0100

    testweise Uebertragung in die Datenbank

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-
 from data_import import faecherliste, schuelernamensliste, jahr_halbjahr_klasse
 
 dateipfad = "/Users/georgsteiner1987/Desktop/notenanalyse/noten_10a.xlsx"
 
-print(jahr_halbjahr_klasse(dateipfad))
-print(schuelernamensliste(dateipfad))
-print(faecherliste(dateipfad))
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "dbrks761",
+    database = "datensammlung"
+)
+mycursor = mydb.cursor()
+mycursor.execute("select * from noten_test")
+
+myresult = mycursor.fetchall()
+for x in myresult:
+  print(x)
+
+
+#print(jahr_halbjahr_klasse(dateipfad))
+#print(schuelernamensliste(dateipfad))
+#print(faecherliste(dateipfad))
